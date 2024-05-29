d4aa6f40bb67eccdc544d71f379736699f0da41a
yousof-ali <yousofali293@gmail.com>
2024-05-30 01:14:23 +0600

    react awesome component spinner

--- src/component/phone/Phone.jsx
+++ src/component/phone/Phone.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { ColorRing } from "react-loader-spinner";
 import { Bar, BarChart, Tooltip, XAxis, YAxis } from "recharts";
 
 const Phone = () => {
     const[phone,setphone]=useState([])
+    const[spin,setspin]=useState(true)
     useEffect(()=>{
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
         .then(data=>{
@@ -16,17 +18,36 @@ const Phone = () => {
                 return obj
             })
             setphone(newob)
+            setspin(false)
             
         })
     },[])
     return (
 
-        <BarChart width={1900} height={400} data={phone}>
+        
+
+        <div>
+            <div>
+                
+                { spin && 
+                    <ColorRing
+                        visible={true}
+                        height="80"
+                        width="80"
+                        ariaLabel="color-ring-loading"
+                        wrapperStyle={{}}
+                        wrapperClass="color-ring-wrapper"
+                        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+                     />
+                }
+            </div>
+            <BarChart width={1900} height={400} data={phone}>
             <Bar dataKey={'price'}></Bar>
             <XAxis dataKey={'name'}></XAxis>
             <YAxis></YAxis>
             <Tooltip></Tooltip>
-        </BarChart>
+           </BarChart>
+        </div>
 
 
     );
